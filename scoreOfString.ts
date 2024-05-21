1fb70ca84e9e4e52cb9adc36533cc9a43780128e
rodyherrera <contact@rodyherrera.com>
2024-05-21 18:27:48 -0400

    feat: scoreOfString.ts

---
+++ scoreOfString.ts
@@ -0,0 +1,7 @@
+const scoreOfString = (s: string): number => {
+    let score = 0;
+    for(let i = 1; i < s.length; i++){
+        score += Math.abs(s.charCodeAt(i) - s.charCodeAt(i - 1));
+    }
+    return score;
+};
