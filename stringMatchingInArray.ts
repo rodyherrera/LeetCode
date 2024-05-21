00bb65087d2d880366bc93bc0d48d60f38c35d2b
rodyherrera <contact@rodyherrera.com>
2024-05-21 18:31:16 -0400

    feat: stringMatchingInArray.ts

---
+++ stringMatchingInArray.ts
@@ -0,0 +1,12 @@
+const stringMatching = (words: string[]): string[] => {
+    const result: string[] = [];
+    for(let i = 0; i < words.length; i++){
+        for(let j = 0; j < words.length; j++){
+            if(i !== j && words[j].includes(words[i])){
+                result.push(words[i]);
+                break;
+            }
+        }
+    }
+    return result;
+};
