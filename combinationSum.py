0e2d69b07d80441ca506576050db0f306c64c21e
rodyherrera <contact@rodyherrera.com>
2024-05-21 23:49:23 -0400

    feat: combinationSum.py

--- combinationSum.py
+++ combinationSum.py
@@ -2,19 +2,16 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
         result = []
-        
         def backtrack(combination, start, target):
             if target == 0:
                 result.append(combination[:])
                 return
-            
             for i in range(start, len(candidates)):
-                # Optimization: If current candidate exceeds the target, break
+                # Optimization if current candidate exceeds the target, break
                 if candidates[i] > target:
                     break
                 combination.append(candidates[i])
-                backtrack(combination, i, target - candidates[i])  # Use the same candidate again
-                combination.pop()  # Backtrack
-                
+                backtrack(combination, i, target - candidates[i])
+                combination.pop()
         backtrack([], 0, target)
         return result
